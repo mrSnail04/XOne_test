a421711222034c3d3ed3a70a0dff8a03b9281ee9
Maksim Bulauski <maks.bulavsky@gmail.com>
2022-07-12 18:18:18 +0300

    heroku

--- entrypoint-prod.sh
+++ entrypoint-prod.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python backend/manage.py makemigrations --no-input
-python backend/manage.py migrate --no-input
-python backend/manage.py runserver 0.0.0.0:$PORT
+python manage.py makemigrations --no-input
+python manage.py migrate --no-input
+python manage.py runserver 0.0.0.0:$PORT
