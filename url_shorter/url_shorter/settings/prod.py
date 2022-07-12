6c737a6a111a28c478b9485d53daffe1b9d65a01
Maksim Bulauski <maks.bulavsky@gmail.com>
2022-07-12 17:58:18 +0300

    heroku

--- url_shorter/url_shorter/settings/prod.py
+++ url_shorter/url_shorter/settings/prod.py
@@ -1,10 +1,9 @@
-import os
 import dj_database_url
 from .base import *
 
 SECRET_KEY = 'django-insecure-f+^ndv5sm(#8ts7$ucy^zuij6)v450-eoa08!@7!4zha#c)fb^'
 DEBUG = False
-ALLOWED_HOSTS = ['ancient-oasis-20487.herokuapp.com']
+ALLOWED_HOSTS = ['xonetest.herokuapp.com']
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
 db_from_env = dj_database_url.config(
